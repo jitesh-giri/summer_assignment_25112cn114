a404d680c410561b5f5f7ee7d0ef0d1f00df7a86
Jitesh Giri <jitishgiri@gmail.com>
2026-06-07 14:41:09 +0530

    Create q_25.java

---
+++ DAY_7/q_25.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class RecursiveFactorial {
+
+    static long factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial = " + factorial(n));
+    }
+}
